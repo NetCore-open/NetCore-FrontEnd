6eb87c656e915e7977f7d4f6e397ff7d7a126eb2
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-05-07 15:27:24 -0500

    feat:configurar polyfills en angular.json para habilitar zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http'; // <-- Importar esto
+import { provideHttpClient, withFetch } from '@angular/common/http'; // Añadimos withFetch
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    // Esto optimiza cómo Angular detecta cambios (y quita el error NG0908)
     provideZoneChangeDetection({ eventCoalescing: true }),
+
     provideRouter(routes),
-    provideHttpClient() // <-- Agregarlo aquí
+
+    // Configuramos el cliente HTTP con Fetch API para mejor rendimiento
+    provideHttpClient(withFetch())
+
+    /* A futuro, cuando tengamos el interceptor de seguridad,
+       lo añadiremos aquí como en tu ejemplo.
+    */
   ]
 };
