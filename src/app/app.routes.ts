393cd3969dda7539e05986daeddcbbe4d74174ad
GabrielMendoza18 <gabrielmendoza3746@gmail.com>
2026-05-09 19:05:31 -0500

    feat(billing): registrar rutas protegidas y finalizar integracion del BC

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { SignInComponent } from './users/presentation/views/sign-in/sign-in';
 import { SignUpComponent } from './users/presentation/views/sign-up/sign-up';
 import { Dashboard } from './layout/views/dashboard/dashboard';
 import { authGuard } from './users/application/auth.guard';
+import { PlansComponent } from './billing/presentation/views/plans/plans';
+import { SubscriptionsComponent } from './billing/presentation/views/subscriptions/subscriptions';
 // Asumiendo que tienes o crearás este para Carlos
 // import { InventoryComponent } from './admin/views/inventory/inventory';
 
@@ -23,6 +25,18 @@ export const routes: Routes = [
     path: 'admin/inventory',
     // component: InventoryComponent,
     component: Dashboard, // Uso Dashboard temporalmente para que no te de error si no tienes el otro
+    canActivate: [authGuard(['ADMIN'])]
+  },
+
+  // 💳 Rutas de Billing (solo ADMIN)
+  {
+    path: 'admin/plans',
+    component: PlansComponent,
+    canActivate: [authGuard(['ADMIN'])]
+  },
+  {
+    path: 'admin/subscriptions',
+    component: SubscriptionsComponent,
     canActivate: [authGuard(['ADMIN'])]
   },
 
