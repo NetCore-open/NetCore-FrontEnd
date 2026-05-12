1f1087218931007adc0a7dbac1f6f5e00a238f35
GabrielMendoza18 <gabrielmendoza3746@gmail.com>
2026-05-11 22:37:49 -0500

    feat(billing): UI update for plans and subscriptions

--- src/app/billing/presentation/views/subscriptions/subscriptions.ts
+++ src/app/billing/presentation/views/subscriptions/subscriptions.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { BillingStore } from '../../../application/billing.store';
 
 @Component({
   selector: 'app-subscriptions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './subscriptions.html',
   styleUrl: './subscriptions.css'
 })
